6b87bf4aa3aa9590fa943919ac8a2fbec3f571ae
dlwjddhks <1104jungwan@naver.com>
2026-06-25 19:35:36 +0900

    🎉 Start: 프로젝트 생성

---
+++ src/main/java/com/bodeum/BodeumApplication.java
@@ -0,0 +1,13 @@
+package com.bodeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodeumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BodeumApplication.class, args);
+    }
+
+}
