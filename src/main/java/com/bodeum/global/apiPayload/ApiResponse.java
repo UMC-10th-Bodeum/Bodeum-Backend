8d2e139eefa48449969f961d12ed496731d242bf
dlwjddhks <1104jungwan@naver.com>
2026-06-29 17:24:59 +0900

    feat: Swagger, 응답 통일 객체, 에러 핸들러 세팅

---
+++ src/main/java/com/bodeum/global/apiPayload/ApiResponse.java
@@ -0,0 +1,36 @@
+package com.bodeum.global.apiPayload;
+
+import com.bodeum.global.apiPayload.code.BaseErrorCode;
+import com.bodeum.global.apiPayload.code.GeneralSuccessCode;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+
+    @JsonProperty("code")
+    private final String code;
+
+    @JsonProperty("message")
+    private final String message;
+
+    @JsonProperty("result")
+    private T result;
+
+    // 성공한 경우 (result 포함)
+    public static <T> ApiResponse<T> onSuccess(T result) {
+        return new ApiResponse<>(true, GeneralSuccessCode.OK.getCode(), GeneralSuccessCode.OK.getMessage(), result);
+    }
+
+    // 실패한 경우 (result 포함)
+    public static <T> ApiResponse<T> onFailure(BaseErrorCode code, T result) {
+        return new ApiResponse<>(false, code.getCode(), code.getMessage(), result);
+    }
+}
