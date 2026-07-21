de678313223b97e4874db90e38000eed68ec584f
slwnt31 <slwnt8531@gmail.com>
2026-07-21 17:19:11 +0900

    style: 미사용 import 문 제거 #77

--- src/main/java/com/bodeum/domain/info/dto/response/InfoReviewResponse.java
+++ src/main/java/com/bodeum/domain/info/dto/response/InfoReviewResponse.java
@@ -4,7 +4,6 @@ import com.bodeum.domain.info.entity.InfoReview;
 import com.bodeum.domain.info.entity.InfoReviewImage;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public record InfoReviewResponse(
