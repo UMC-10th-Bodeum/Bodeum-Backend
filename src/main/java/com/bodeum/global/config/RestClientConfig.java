600db16f2151c532e7b78bcc9b99d4c28ffa6a6c
slwnt31 <slwnt8531@gmail.com>
2026-07-29 20:48:47 +0900

    refactor: import 문 삭제 #139

--- src/main/java/com/bodeum/global/config/RestClientConfig.java
+++ src/main/java/com/bodeum/global/config/RestClientConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class RestClientConfig {
