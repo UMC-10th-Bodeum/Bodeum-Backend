cddfd933b37483295f13331f49296e13ee589977
slwnt31 <slwnt8531@gmail.com>
2026-08-06 22:12:10 +0900

    style : import 문 제거 #246

--- src/main/java/com/bodeum/domain/info/repository/InfoItemRepository.java
+++ src/main/java/com/bodeum/domain/info/repository/InfoItemRepository.java
@@ -14,7 +14,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 
 @Repository
 public interface InfoItemRepository extends JpaRepository<InfoItem, Long>, InfoItemRepositoryCustom {
