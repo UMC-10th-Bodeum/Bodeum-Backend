ca35d14925aff942da78b50fbc47b637177fb610
dppfls <a93864140@gmail.com>
2026-08-07 16:23:50 +0900

    perf: #243 스크랩 게시글 해시태그 조회 N+1 개선

--- src/main/java/com/bodeum/domain/community/repository/PostHashtagRepository.java
+++ src/main/java/com/bodeum/domain/community/repository/PostHashtagRepository.java
@@ -2,12 +2,14 @@ package com.bodeum.domain.community.repository;
 
 import com.bodeum.domain.community.entity.PostHashtag;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostHashtagRepository extends JpaRepository<PostHashtag, Long> {
 
     List<PostHashtag> findAllByPost_IdOrderByIdAsc(Long postId);
 
+    @EntityGraph(attributePaths = "hashtag")
     List<PostHashtag> findAllByPost_IdIn(List<Long> postIds);
 
     void deleteAllByPost_Id(Long postId);
